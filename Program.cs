eb9c652346af5260a4e048e5f338e0d773623211
Vera Krasulina <kshisinka@hotmail.com>
2022-10-03 17:23:39 +0300

    Added all needed files

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//To have different sized arrays, we ask to put array elements from user
+Console.WriteLine ("Введите значения массива через пробел:");
+string[] imputArray = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine("Вводный массив:");
+
+// Method, to print massives within this task
+void PrintStringArray (string[] imput)
+{
+
+    Console.Write ("=> [ ");
+    for (int i = 0; i < imput.Length; i++)
+    {
+        Console.Write (imput[i] + " ");
+    }
+    Console.Write ("]");
+}
+
+// Printing imput array
+PrintStringArray (imputArray);
+Console.WriteLine();
